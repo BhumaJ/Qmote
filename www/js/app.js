7f4f240ef3a5a314c6ee596313abea1c309439b1
BhumaJ <jay.savsani07@gmail.com>
2016-02-03 17:59:42 +0530

    new navigation files added

--- www/js/app.js
+++ www/js/app.js
@@ -6,68 +6,85 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            /*if (window.cordova && window.cordova.plugins.Keyboard) {
+             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+             cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
+             } */
+            0
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'views/menu.html',
+                controller: 'AppCtrl'
+            })
+
+            .state('app.remote', {
+                url: '/remote',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'views/remote.html'
+                    }
+                }
+            })
+
+            .state('app.movies', {
+                url: '/movies',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'views/Movies/movies.html'
+                    }
+                }
+            })
+
+            .state('app.tvshows', {
+                url: '/tvshows',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'views/TVshows/tvshows.html'
+                    }
+                }
+            })
+            .state('app.music', {
+                url: '/music',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'views/Music/music.html'
+                    }
+                }
+            })
+            .state('app.setting', {
+                url: '/setting',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'views/setting.html'
+                    }
+                }
+            })
+            .state('app.addons', {
+                url: '/addons',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'views/addons.html'
+                    }
+                }
+            })
+
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/remote');
+    });
